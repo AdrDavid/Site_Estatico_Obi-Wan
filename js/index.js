eae0ce8b5bd608cad378e848963c46e67b366d5f
Adriano Batista <batdavid70@gmail.com>
2023-04-02 17:49:28 -0400

    Site Estatico ObiWan

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ function nativascroll(distanciaDotopo) {
 }
 function scrollToSection(event){
     event.preventDefault();
-    const distanciaTop = distanciaDotopo(event.target) - 120;
+    const distanciaTop = distanciaDotopo(event.target) - 100;
     nativascroll(distanciaTop);
 
 }
@@ -22,3 +22,24 @@ function scrollToSection(event){
 menuLinks.forEach((link) =>{
     link.addEventListener('click', scrollToSection);
 })
+
+const menu = document.querySelector('#menus')
+menu.addEventListener('click',function(){
+   let menuMobile = document.querySelector('.menuMobile')
+    if(menuMobile.classList.contains('none')){
+        menuMobile.classList.remove('none')
+        menuMobile.classList.add('flex')
+    }else if(menuMobile.classList.contains('flex')){
+        menuMobile.classList.remove('flex')
+        menuMobile.classList.add('none')
+    }
+})
+
+var li = document.querySelectorAll('.li')
+for(i = 0; i < li.length; i++) {
+    li[i].addEventListener('click',function(){
+        let menuMobile = document.querySelector('.menuMobile')
+        menuMobile.classList.remove('flex')
+        menuMobile.classList.add('none')
+    })
+}
